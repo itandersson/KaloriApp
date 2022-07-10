7c81268614085fb7d08704c25fc063c960833606
Joakim Andersson <joakim@ktv-sjobo.se>
2022-07-10 15:52:49 +0200

    Calculates calories on a particular food item and send to view

---
+++ ApiHelper/CalorieContent.cs
@@ -0,0 +1,15 @@
+﻿namespace KaloriApp.ApiHelper
+{
+    public class CalorieContent
+    {
+        public string? _id { get; set; }
+        public string? vara { get; set; }
+        public int kj { get; set; }
+        public int kcal { get; set; }
+        public int fett { get; set; }
+        public int kolhydrat { get; set; }
+        public int protein { get; set; }
+        public string? salt { get; set; }
+        public int __v { get; set; }
+    }
+}
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using KaloriApp.Models;
+using KaloriApp.ApiHelper;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Newtonsoft.Json;
 
 namespace KaloriApp.Controllers
 {
@@ -8,9 +10,57 @@ namespace KaloriApp.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
+        String uri = "http://localhost:3000/calculate_calorie_content/";
+        HttpClient client;
+
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            client = new HttpClient();
+            client.BaseAddress = new Uri(uri);
+        }
+
+        /// <summary>
+        /// Call the api of a food item and return
+        /// </summary>
+        /// <param name="food">Get information about this food</param>
+        /// <returns>Information about a food item</returns>
+        private CalorieContent GetAPI(string food)
+        {
+            CalorieContent foodItem = new CalorieContent();
+
+            // Call the api and handle exceptions
+            try
+            {
+                HttpResponseMessage response = client.GetAsync(client.BaseAddress + food).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = response.Content.ReadAsStringAsync().Result;
+                    var data = JsonConvert.DeserializeObject<CalorieContent>(responseBody);
+                    if (data is not null) { foodItem = data; }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+            }
+
+            return foodItem;
+        }
+
+        //Verify that string is not null and is lowercase
+        private string verifyInput(string test)
+        {
+            string verified = "";
+
+            //If not null lower case
+            if (test is not null) 
+            {
+                verified = test.ToLower();
+            }
+
+            return verified;
         }
 
         public IActionResult Index()
@@ -18,15 +68,22 @@ namespace KaloriApp.Controllers
             return View();
         }
 
-        public IActionResult Calories(CaloriesViewModel calories)
+        /// <summary>
+        /// Calculates calories on a particular food
+        /// </summary>
+        /// <param name="caloriesViewModel">A food and its weight</param>
+        /// <returns>Calorie content of a food</returns>
+        public IActionResult Calories(CaloriesViewModel caloriesViewModel)
         {
-            //CaloriesViewModel calories = new CaloriesViewModel();
-            //calories.Gram = 100;
-            //calories.Food = "Socker";
-            calories.Calorie = 400;
+            string food = verifyInput(caloriesViewModel.Food);
+            CalorieContent foodItem = GetAPI(food);
+            
+            // Calculate and set the calorie content of a food
+            float kcal = (caloriesViewModel.Gram) * ( (float)foodItem.kcal / (float)100 );
+            Console.WriteLine(kcal);
+            caloriesViewModel.Calorie = (int)kcal;
 
-            return View(calories);
-
+            return View(caloriesViewModel);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
